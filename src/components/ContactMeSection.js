0124d1a153224d857c6e77e66bb5ddddd1357411
Chitesh Goyal <dv.chitesh97@gmail.com>
2025-07-29 13:02:42 +0530

    Removed unwanted imports

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,23 +1,9 @@
-import React, { useEffect } from "react";
-import { useFormik } from "formik";
 import {
   Box,
-  Button,
-  FormControl,
-  FormHelperText,
-  FormErrorMessage,
-  FormLabel,
   Heading,
-  Input,
-  Select,
-  Textarea,
   VStack,
   HStack
 } from "@chakra-ui/react";
-import * as Yup from 'yup';
-import FullScreenSection from "./FullScreenSection";
-import useSubmit from "../hooks/useSubmit";
-import { useAlertContext } from "../context/alertContext";
 import ContactMeSocials from "./ContactMeSocials";
 import ContactMeForm from "./ContactMeForm";
 
