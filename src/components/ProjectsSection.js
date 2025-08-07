9a0259a4f6fc5bd2c18cd3b51accfc9faf16df7c
Chitesh Goyal <dv.chitesh97@gmail.com>
2025-08-07 23:05:30 +0530

    Update ProjectsSection.js

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,35 +1,44 @@
 import React from "react";
-import FullScreenSection from "./FullScreenSection";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
 import Card from "./Card";
 import { projects } from "../data";
 
 const ProjectsSection = () => {
   return (
-    <FullScreenSection
-      p={8}
-      alignItems="flex-start"
-      spacing={8}
-      id="projects-section"
-    >
-      <Heading as="h1">
-        Featured Projects
-      </Heading>
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(2,minmax(0,1fr))"
-        gridGap={8}
-      >
-        {projects.map((project) => (
-          <Card
-            key={project.title}
-            title={project.title}
-            description={project.description}
-            imageSrc={project.getImageSrc()}
-          />
-        ))}
-      </Box>
-    </FullScreenSection>
+    <Box
+      pt={16}
+      pb={64}
+      px={8}
+      display="flex"
+      justifyContent="center"
+      id="projects-section">
+      <VStack w="100%">
+        <Heading
+          as="h2"
+          color="purple.500"
+          size="2xl"
+          lineHeight={2}
+          pb="8"
+          pt="8"
+        >
+          Featured projects
+        </Heading>
+        <Box
+          display="grid"
+          gridTemplateColumns="repeat(2,minmax(0,1fr))"
+          gridGap={8}
+        >
+          {projects.map((project) => (
+            <Card
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              imageSrc={project.getImageSrc()}
+            />
+          ))}
+        </Box>
+      </VStack>
+    </Box>
   );
 };
 
